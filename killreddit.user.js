e18f16ceb180dd305b708c7b47e155fb235a9ace
keythkatz <github@jerl.im>
2016-04-04 17:56:22 +0800

    Create killreddit.user.js

---
+++ killreddit.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Kill Reddit More
+// @namespace    http://jerl.im
+// @version      1.0
+// @description  Autoreload Reddit when it dies
+// @author       You
+// @match        *.reddit.com/*
+// @grant        none
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+if(document.querySelectorAll("img[src='//www.redditstatic.com/trouble-afoot.jpg']").length > 0) window.location.reload();
