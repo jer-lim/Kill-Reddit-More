58620ab0621f657bf77362b0b64cfa647d4c338f
keythkatz <github@jerl.im>
2016-05-25 21:19:57 +0800

    Update killreddit.user.js

--- killreddit.user.js
+++ killreddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kill Reddit More
 // @namespace    http://jerl.im
-// @version      1.1
+// @version      1.2
 // @description  Autoreload Reddit when it dies
 // @author       You
 // @match        *.reddit.com/*
@@ -11,4 +11,4 @@
 'use strict';
 
 if(document.querySelectorAll("img[src='//www.redditstatic.com/trouble-afoot.jpg']").length > 0) window.location.reload();
-if(document.title = "reddit broke!") window.location.reload();
+if(document.title == "reddit broke!") window.location.reload();
