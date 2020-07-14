be9c46ca6cf7e24ed398c93b4a988e7bb0d91e7c
Jeremy Lim <me@jerl.im>
2020-07-14 18:03:32 +0800

    Update killreddit.user.js

--- killreddit.user.js
+++ killreddit.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kill Reddit More
 // @namespace    http://jerl.im
-// @version      1.3
+// @version      1.3.1
 // @description  Autoreload Reddit when it dies
 // @author       You
 // @match        *.reddit.com/*
@@ -12,4 +12,4 @@
 
 if(document.querySelectorAll("img[src='//www.redditstatic.com/trouble-afoot.jpg']").length > 0) window.location.reload();
 if(document.title == "reddit broke!") window.location.reload();
-else if(document.title == "Ow! -- reddit.com!") window.location.reload();
+else if(document.title == "Ow! -- reddit.com") window.location.reload();
