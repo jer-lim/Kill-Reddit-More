904ab19e3729a115780f122c7abaa5c8198323f1
Jeremy Lim <github@jerl.im>
2022-01-03 05:16:53 +0800

    Update killreddit.user.js

--- killreddit.user.js
+++ killreddit.user.js
@@ -14,6 +14,7 @@ var reload = false;
 if (document.querySelectorAll("img[src='//www.redditstatic.com/trouble-afoot.jpg']").length > 0) reload = true;
 else if (document.title == "reddit broke!") reload = true;
 else if (document.title == "Ow! -- reddit.com") reload = true;
+else if (document.title == "504 Gateway Timeout") reload = true;
 else if (document.documentElement.outerText.substr(0, 18) === "connection failure") reload = true;
 
 if (reload) window.location.reload();
